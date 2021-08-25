d9d980cd6dcfe2072612193a938e2d3c279e0e87
300073043 <yash.tyagi@myntra.com>
2021-08-25 12:47:13 +0530

    commit #2 with image downloader

--- hashtag.py
+++ hashtag.py
@@ -4,6 +4,7 @@ import constants
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import urllib.request
 
 
 def load_fetch_post(driver):
@@ -27,33 +28,23 @@ def load_fetch_post(driver):
             if img not in image_list:
                 print(img)
                 image_list.append(img)
-
-        print(image_list)
-        if no_of_posts > 12:  # 12 posts loads up when we open the profile
-            no_of_scrolls = 1  # extra scrolls if any error occurs while scrolling.
-
-            # Loading all the posts
-            print('Loading all the posts...')
-            for __ in range(no_of_scrolls):
-
-                # Every time the page scrolls down we need to get the source code as it is dynamic
-                driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-                sleep(2)  # introduce sleep time as per your internet connection as to give the time to posts to load
-
-                soup = BeautifulSoup(driver.page_source, 'lxml')
-                all_images = soup.find_all('img')
-                type(all_images)
-                new_images=[]
-                all_images_text=[str(img).split('src')[1].split('style')[0] for img in all_images]
-
-                for img in all_images_text:
-                    new_images.append((img.split("src")[1].split("style")[0]))
-                print(new_images)
-
-
-                for img in all_images:
-                    if img not in image_list:
-                        image_list.append(img)
+        image_list_src=[]
+        # print('---'+image_list+'----')
+        for img in image_list:
+            try:
+                image_list_src.append(str(img).split('src')[1].split('style')[0].replace('amp;','').lstrip("'").lstrip('=').lstrip('"').rstrip(' ').rstrip('"'))
+            except Exception:
+                print('error in getting images')
+        print('###################')
+        print(image_list_src)
+        print('##################')
+        for i in range(len(image_list_src)):
+            try:
+                urllib.request.urlretrieve(image_list_src[i], "/Users/300073043/Desktop/hack/images/"+str(i)+".jpg")
+                print('downloaded '+str(i))
+            except Exception:
+                print('Exception occured for '+str(i))
+        print(image_list_src)
     except Exception:
         print('Some error occurred while scrolling down and trying to load all posts.')
         sys.exit()
@@ -75,9 +66,9 @@ def userLogin():
     driver.find_element_by_css_selector(
         'body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm').click()
     driver.get(constants.komalPandey)
-
+    sleep(3)
     l= load_fetch_post(driver)
-    with open("out.csv","w") as f:
+    with open("image_url.csv","w") as f:
         wr = csv.writer(f,delimiter="\n")
         wr.writerow(l)
 
